b680af9752f3a19c4df190bedc50b021640c1e61
macorona <A01370358@tec.mx>
2026-02-20 20:51:03 -0600

    Se agrega funcionalidad de los análisis de código estatico con pyling y flake8 y la evidencia del proceso.

--- Proyecto/reserva_hotel.py
+++ Proyecto/reserva_hotel.py
@@ -1,55 +1,56 @@
+"""Modulo de reserva hotelera"""
+
+from pathlib import Path
 from servicios import HotelService, CustomerService, ReservationService
 from storage import JsonStorage
-from pathlib import Path
 
-"""Run example workflow."""
 
 script_location = Path(__file__).parent
 ARCHIVO_HOTELES = 'data/hotels.json'
 file_location = script_location / ARCHIVO_HOTELES
-#print(file_location)
+# print(file_location)
 # Initialize storage
 hotel_storage = JsonStorage(file_location)
 
-## Hoteles ##
+# Hoteles
 # Initialize services
 print("=== Mostrar Hoteles ===")
 hotel_service = HotelService(hotel_storage)
 hotel_service.show_hotels()
 
-#print("=== Crear un Hotel ===")
-#hotel = hotel_service.create_hotel(
-#    name="Marriot Cancun",
-#    location="México",
-#    total_rooms=5
-#)
-#print(hotel)
+print("=== Crear un Hotel ===")
+hotel = hotel_service.create_hotel(
+    name="Marriot Cancun",
+    location="México",
+    total_rooms=5
+)
+# print(hotel)
 
-#print("=== Actualizar Hotel ===")
-#id_hotel =  "d7d9b2b6-0c38-4754-b2a3-1a7b24c42637"
-#hotel_service.update_hotel(
-#    hotel_id=id_hotel,
-#    name="Marriot Los Cabos",
-#    location="México",
-#    total_rooms=6
-#)
+print("=== Actualizar Hotel ===")
+ID_HOTEL = "d7d9b2b6-0c38-4754-b2a3-1a7b24c42637"
+hotel_service.update_hotel(
+    hotel_id=ID_HOTEL,
+    name="Marriot Los Cabos",
+    location="México",
+    total_rooms=6
+)
 
-#print("=== Eliminar un Hotel ===")
-#id_hotel =  "g7d9b2b6-0c38-4754-b2a3-1a7b24c42645"
-#hotel_service.delete_hotel(id_hotel)
+print("=== Eliminar un Hotel ===")
+ID_HOTEL = "g7d9b2b6-0c38-4754-b2a3-1a7b24c42645"
+hotel_service.delete_hotel(ID_HOTEL)
 
-#print("=== Mostrar Hoteles ===")
-#hotel_service.show_hotels()
+print("=== Mostrar Hoteles ===")
+hotel_service.show_hotels()
 
-## Clientes ##
+# Clientes
 ARCHIVO_CLIENTES = 'data/clientes.json'
 file_location = script_location / ARCHIVO_CLIENTES
 # Initialize storage
 customer_storage = JsonStorage(file_location)
-## Hoteles ##
+# Hoteles
 # Initialize services
 customer_service = CustomerService(customer_storage)
-#customer_service.show_customers()
+# customer_service.show_customers()
 
 print("=== Agregar un cliente ===")
 customer = customer_service.create_customer(
@@ -59,43 +60,47 @@ customer = customer_service.create_customer(
 
 customer_service.show_customers()
 
-#print("=== Actualizar Cliente ===")
-#id_cliente =  "C002"
-#customer_service.update_customer(
-#    customer_id=id_cliente,
-#    name="Roberto Canales",
-#    email="canales@example.com"
-#)
+print("=== Actualizar Cliente ===")
+ID_CLIENTE = "C002"
+customer_service.update_customer(
+    customer_id=ID_CLIENTE,
+    name="Roberto Canales",
+    email="canales@example.com"
+)
 
-#print("=== Eliminar un Cliente ===")
-#id_cliente =  "C001"
-#customer_service.delete_customer(id_cliente)
+print("=== Eliminar un Cliente ===")
+ID_CLIENTE = "C001"
+customer_service.delete_customer(ID_CLIENTE)
 
-## Reservaciones ##
+# Reservaciones
 ARCHIVO_RESERVAS = 'data/reservas.json'
 file_location = script_location / ARCHIVO_RESERVAS
-# Initialize storage    
+
+# Initialize storage
 reservation_storage = JsonStorage(file_location)
+
 # Initialize services
 reservation_service = ReservationService(
     reservation_storage,
     hotel_service,
     customer_service
 )
+
 print("=== Crear una reservación ===")
-id_hotel =  "g7d9b2b6-0c38-4754-b2a3-1a7b24c42645"
+ID_HOTEL = "g7d9b2b6-0c38-4754-b2a3-1a7b24c42645"
 reservacion = reservation_service.create_reservation(
         customer.customer_id,
-        id_hotel
+        ID_HOTEL
 )
+
 print(reservacion)
 print("\n=== Hotel después de la reservación ===")
-updated_hotel = hotel_service.get_hotel(id_hotel)
+updated_hotel = hotel_service.get_hotel(ID_HOTEL)
 print(updated_hotel)
 
 print("\n=== Cancelar la reservación ===")
 reservation_service.cancel_reservation(reservacion.reservation_id)
 
 print("\n=== Hotel después de la reservación cancelada ===")
-updated_hotel = hotel_service.get_hotel(id_hotel)
+updated_hotel = hotel_service.get_hotel(ID_HOTEL)
 print(updated_hotel)
